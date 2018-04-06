f92fcbc59943597ec2af272e3a446d5017b1e11a
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-04-06 11:27:16 -0400

    created attention router

--- server/api/attention.js
+++ server/api/attention.js
@@ -1,1 +1,49 @@
 const router = require('express').Router()
+const { Attention } = require('../db/models')
+module.exports = router
+
+/* get all attention entries for a single child */
+router.get('/:childId', (req, res, next) => {
+  Attention.findAll({
+    where: {
+      childId: req.params.childId
+    }
+  })
+  .then(attentionEntries => {
+    res.json(attentionEntries)
+  })
+  .catch(next)
+})
+
+/* create a new attention entry for a child */
+router.post('/:childId', (req, res, next) => {
+  Attention.create(req.body)
+    .then(newAttentionEntry => {
+      res.json(newAttentionEntry)
+    })
+    .catch(next)
+})
+
+/* update an existing attention entry */
+router.put('/:attentionId', (req, res, next) => {
+  Attention.update(req.body, {
+    where: {
+      id: req.params.attentionId
+    }, returning: true
+  })
+  .then(updatedAttentionEntry => {
+    res.json(updatedAttentionEntry)
+  })
+  .catch(next)
+})
+
+/* destroy an existing attention entry */
+router.delete('/:attentionId', (req, res, next) => {
+  Attention.destroy({
+    where: {
+      id: req.params.attentionId
+    }
+  })
+  .then(() => res.status(204).send('Entry successfully deleted'))
+  .catch(next)
+})
